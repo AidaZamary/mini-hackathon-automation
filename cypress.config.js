06370aec6bc44dff77ed4d3515f838360bf2585b
AidaZamary <itsaidazmry@gmail.com>
2025-07-11 06:41:59 +0800

    add mocha

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
   },
 });
