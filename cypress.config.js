22b4e67c49517e4b826adbe2c5b4355f43d84f6c
AidaZamary <itsaidazmry@gmail.com>
2025-07-12 08:49:29 +0800

    lambdatest

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,16 @@
 const { defineConfig } = require("cypress");
-const mochawesome = require('cypress-mochawesome-reporter/plugin');
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports/mochawesome',
-    overwrite: false,
-    html: false,
-    json: true
-  },
+  reportDir: "cypress/reports/mochawesome",
+  overwrite: false,
+  html: false,
+  json: true
+},
   e2e: {
     setupNodeEvents(on, config) {
-      mochawesome(on); // ✅ Required for reporter to work
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
     }
   }
